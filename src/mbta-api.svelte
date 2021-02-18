0737a926869542a5c43242cd9fdcf2934b80a388
ncreel <nick.creel@protonmail.com>
2021-02-18 13:23:07 -0500

    fetch alerts for all lines (another test)

--- src/mbta-api.svelte
+++ src/mbta-api.svelte
@@ -3,7 +3,7 @@
 
   let query = "alerts"
   const apiKey = "9692d1a17a814d86822248b3ee1b339d";
-  const apiURL = `https://api-v3.mbta.com/${query}?api_key=${apiKey}&sort=description&filter%5Bactivity%5D=BOARD%2CEXIT%2CRIDE&filter%5Broute_type%5D=0&filter%5Broute%5D=Orange`;
+  const apiURL = `https://api-v3.mbta.com/${query}?api_key=${apiKey}&sort=description&filter%5Bactivity%5D=BOARD%2CEXIT%2CRIDE&filter%5Broute_type%5D=0&filter%5Broute%5D=Orange,Blue,Green,Red,Silver`;
   let object = {};
 
   onMount(async function(){
@@ -15,6 +15,7 @@
 </script>
 
 {#if typeof object.data !== 'undefined'}
+<!--Need to wait until object is fetched and assigned or this will fail-->
   <div class="data-display">
     {#each object.data as alert}
       <div class="data-item">
